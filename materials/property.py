84e6ae0bc6477999f115e69b6eaadca8b67aeb2f
Matthew Vernacchia <mvernacc@mit.edu>
2019-02-12 14:43:47 -0500

    Working prototype of Properties class.

--- materials/property.py
+++ materials/property.py
@@ -30,25 +30,25 @@ class StateDependentProperty(Property):
                 self._interp_points = np.empty((0, 2))
                 self._interp_values = np.array([])
                 tbl_dict = yaml_dict['variation_with_state'][self.state_vars[0]]
-                interp_points_0 = np.array(tbl_dict.keys())
+                interp_points_0 = np.array(list(tbl_dict.keys()))
                 interp_points_0.sort()
                 interp_points_1 = np.array(
                     [tbl_dict[p0][self.state_vars[1]] for p0 in interp_points_0])
                 # From the yaml dictionary 2d table, build an array compatible with
                 # scipy.interpolate.griddata
-                for i in len(interp_points_0):
+                for i in range(len(interp_points_0)):
                     # The value of the 0th state variable for this slice of the inperpolation table.
                     point_0 = interp_points_0[i]
-                    self._interp_points.append(
+                    self._interp_points = np.append(
                         self._interp_points,
-                        np.concatenate(
+                        np.stack(
                             (
-                                np.full_like(interp_points_1[i], point_0),
+                                np.full_like(interp_points_1[i], point_0, dtype=np.double),
                                 interp_points_1[i]
                             ),
                             axis=1),
                         axis=0)
-                    self._interp_values.append(tbl_dict[point_0]['values'])
+                    self._interp_values = np.append(self._interp_values, tbl_dict[point_0]['values'])
             else:
                 raise NotImplementedError('More than two state variables not supported.')
 
@@ -56,7 +56,7 @@ class StateDependentProperty(Property):
             raise NotImplementedError('Representations other than table are not yet supported.')
 
 
-    def query_value(self, state, method='linear', fill_value=np.nan, rescale=False):
+    def query_value(self, state, method='linear', fill_value=np.nan, rescale=True):
         """Query the value of the property at a particular state.
 
         Arguments:
@@ -74,16 +74,17 @@ class StateDependentProperty(Property):
         if len(self.state_vars) == 1:
             query_points = np.array(state[self.state_vars[0]])
         elif len(self.state_vars) == 2:
-            assert len(state[self.state_vars[0]]) == state[self.state_vars[1]]
-            query_points = np.concatenate(
-                (state[self.state_vars[0]], state[self.state_vars[1]]), axis=1)
+            if hasattr(state[self.state_vars[0]], '__len__') \
+            and hasattr(state[self.state_vars[1]], '__len__'):
+                assert len(state[self.state_vars[0]]) == len(state[self.state_vars[1]])
+                query_points = np.stack(
+                    (state[self.state_vars[0]], state[self.state_vars[1]]), axis=1)
+            else:
+                query_points = np.array([[state[self.state_vars[0]], state[self.state_vars[1]]]])
 
         values = scipy.interpolate.griddata(self._interp_points, self._interp_values,
                                             query_points,
                                             method=method, fill_value=fill_value, rescale=rescale)
-        # return a scalar if we were given a scalar
-        # if len(query_points) == 1:
-        #     values = values[0]
 
         if self._value_type == 'multiplier':
             values = self.default_value * values
--- materials/property_test.py
+++ materials/property_test.py
@@ -70,6 +70,102 @@ class TestStateDependentProperty(unittest.TestCase):
         with self.assertRaises(ValueError):
             prop.query_value({'fish': 1.})    # fish is not a state variable.
 
+    def test_init_2d(self):
+        """Test init with a 2-d lookup table."""
+        # Setup
+        yaml_dict = {
+            'default_value': 2.0,
+            'units': 'MPa',
+            'reference': 'mmpds',
+            'variation_with_state': {
+                'state_vars': ['exposure time', 'temperature'],
+                'value_type': 'multiplier',
+                'representation': 'table',
+                'exposure time': {
+                    0.0: {
+                        'temperature': np.arange(4),
+                        'values': np.arange(4)**2
+                    },
+                    0.1: {
+                        'temperature': np.arange(4),
+                        'values': (np.arange(4) + 0.1)**2
+                    },
+                }
+            }}
+
+        # Action
+        prop = StateDependentProperty(yaml_dict)
+
+        # Verification
+        self.assertEqual(2.0, prop.default_value)
+        self.assertEqual('MPa', prop.units)
+        self.assertEqual('mmpds', prop.reference)
+        self.assertEqual(len(prop.state_vars), 2)
+        self.assertEqual(prop.state_vars[0], 'exposure time')
+        self.assertEqual(prop.state_vars[1], 'temperature')
+
+        # Verification of "private" attributes
+        # These tests are tied to the implementation, not interface
+        # Is this sketchy? Should the test only depend on the interface?
+        # pylint: disable=protected-access
+        self.assertEqual(prop._interp_points.shape[0], 4 * 2)
+        self.assertEqual(prop._interp_points.shape[1], 2)
+        self.assertEqual(prop._interp_points[0, 0], 0)
+        self.assertEqual(prop._interp_points[4, 0], 0.1)
+        self.assertEqual(prop._interp_points[0, 1], 0)
+        self.assertEqual(prop._interp_points[3, 1], 3)
+        self.assertEqual(prop._interp_points[4, 1], 0)
+        self.assertEqual(prop._interp_points[7, 1], 3)
+
+    def test_query_2d(self):
+        """Test query_value with a 2-d lookup table."""
+        # Setup
+        dv = 2.0
+        yaml_dict = {
+            'default_value': dv,
+            'units': 'MPa',
+            'reference': 'mmpds',
+            'variation_with_state': {
+                'state_vars': ['exposure time', 'temperature'],
+                'value_type': 'multiplier',
+                'representation': 'table',
+                'exposure time': {
+                    0.0: {
+                        'temperature': np.arange(4),
+                        'values': np.arange(4)**2
+                    },
+                    0.1: {
+                        'temperature': np.arange(4),
+                        'values': (np.arange(4) + 0.5)**2
+                    },
+                }
+            }}
+
+        prop = StateDependentProperty(yaml_dict)
+
+        # Action and verification
+        # Check queries at given points
+        result = prop.query_value({'exposure time': 0, 'temperature': 1})
+        self.assertEqual(result, 1**2 * dv)
+        result = prop.query_value({'exposure time': 0.1, 'temperature': 3})
+        self.assertEqual(result, 3.5**2 * dv)
+        # Check interpolation between given points
+        result = prop.query_value({'exposure time': 0, 'temperature': 2.5}, method='linear')
+        self.assertAlmostEqual(result, 6.5 * dv)
+        result = prop.query_value({'exposure time': 0.05, 'temperature': 2}, method='linear')
+        self.assertAlmostEqual(result, (2**2 + 2.5**2)/2 * dv)
+
+        # Check query of multiple points
+        result = prop.query_value({'exposure time': [0, 0.05], 'temperature': [1, 2]})
+        self.assertEqual(result[0], 1**2 * dv)
+        self.assertAlmostEqual(result[1], (2**2 + 2.5**2)/2 * dv)
+
+        # Check bad query
+        with self.assertRaises(ValueError):
+            prop.query_value({'fish': 1., 'temperature': 1})    # fish is not a state variable.
+        with self.assertRaises(ValueError):
+            prop.query_value({'fish': 1., 'exposure time': 1})    # fish is not a state variable.
+
 
 
 if __name__ == '__main__':
