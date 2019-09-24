145a0f2ec29dd336d52e184c3e4b35c41d3bf58a
Matthew Vernacchia <mvernacc@mit.edu>
2019-09-24 14:03:18 -0400

    Add Stainless steel 316L: annealed sheet and additive.

--- materials/database_test.py
+++ materials/database_test.py
@@ -19,6 +19,26 @@ class TestAISI304(unittest.TestCase):
         print('\n' + str(aisi304) + '\n')
 
 
+class TestAISI316L(unittest.TestCase):
+    """Unit tests for AISI 304."""
+
+    def test_load(self):
+        """Make sure the material loads from the YAML file w/o throwing errors."""
+        # Action
+        aisi316L_sheet = load('AISI_316L', 'sheet', 'annealed')
+        aisi316L_am_renishaw = load('AISI_316L', 'additive, Renishaw', 'as-built')
+        aisi316L_am_eos = load('AISI_316L', 'additive, EOS', 'as-built')
+
+        # Verificaiton
+        self.assertEqual(type(aisi316L_sheet), Material)
+        self.assertEqual(type(aisi316L_am_renishaw), Material)
+        self.assertEqual(type(aisi316L_am_eos), Material)
+        # TODO automate checking this
+        print('\n' + str(aisi316L_sheet) + '\n')
+        print('\n' + str(aisi316L_am_renishaw) + '\n')
+        print('\n' + str(aisi316L_am_eos) + '\n')
+
+
 class TestAl6061(unittest.TestCase):
     """Unit tests for Al 6061."""
 
